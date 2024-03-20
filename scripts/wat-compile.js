27e8be5454a767fe288274e974c7bfdb3987b6ac
Roman Dvornov <rdvornov@gmail.com>
2024-03-20 17:41:31 +0100

    Setup wat compile

---
+++ scripts/wat-compile.js
@@ -0,0 +1,37 @@
+/* eslint-env node */
+const path = require('path');
+const fs = require('fs');
+const wabtPromise = require('wabt')();
+
+const files = [
+    '../app/prepare/build-trees.wat'
+].map(relpath => path.join(__dirname, relpath));
+
+async function compileWatToWasm() {
+    console.log('Compile WAT into WASM');
+    console.log();
+
+    const wabt = await wabtPromise;
+
+    for (let filepath of files) {
+        process.stdout.write(path.relative(process.cwd(), filepath) + '...');
+
+        const destPath = filepath.replace(/\.wat$/, '.wasm');
+        const watFileContent = fs.readFileSync(filepath, 'utf8');
+        const wasmModule = wabt.parseWat(filepath, watFileContent);
+        const { buffer } = wasmModule.toBinary({});
+
+        fs.writeFileSync(destPath, buffer);
+
+        console.log('OK');
+        console.log('  Written to', path.relative(process.cwd(), destPath));
+    }
+}
+
+if (module === require.main) {
+    compileWatToWasm();
+}
+
+module.exports = {
+    compileWatToWasm
+};
