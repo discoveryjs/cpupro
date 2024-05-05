52d4d37cecc93022d3e40c5cfb81d7167f2924a9
Roman Dvornov <rdvornov@gmail.com>
2024-05-05 22:58:16 +0200

    Add stub category switcher

--- app/views/flamechart-expand.js
+++ app/views/flamechart-expand.js
@@ -10,13 +10,32 @@ discovery.view.define('flamechart-expand', function(el, config, data, context) {
         className: 'flamechart-expand trigger-outside',
         header: header || 'text:"Subtrees flame chart"',
         content: {
-            view: 'flamechart',
-            tree: focusTree,
-            timings,
-            timingsMap: focusTree.timingsMap,
-            lockScrolling: true,
-            postRender(el) {
-                el.classList.toggle('lock-scrolling', true);
+            view: 'context',
+            modifiers: {
+                view: 'block',
+                className: 'toolbar',
+                content: [
+                    {
+                        view: 'toggle-group',
+                        name: 'dataset',
+                        data: [
+                            { text: 'Categories', value: 'categoriesTree', active: tree === context.data.categoriesTree },
+                            { text: 'Packages', value: 'packagesTree', active: tree === context.data.packagesTree },
+                            { text: 'Modules', value: 'modulesTree', active: tree === context.data.modulesTree },
+                            { text: 'Functions', value: 'functionsTree', active: tree === context.data.functionsTree }
+                        ]
+                    }
+                ]
+            },
+            content: {
+                view: 'flamechart',
+                tree: focusTree,
+                timings,
+                timingsMap: focusTree.timingsMap,
+                lockScrolling: true,
+                postRender(el) {
+                    el.classList.toggle('lock-scrolling', true);
+                }
             }
         }
     }, data, context);
