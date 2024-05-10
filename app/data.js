403eeb020312be2f336c0605152c13bb5f4c83f2
Roman Dvornov <rdvornov@gmail.com>
2024-05-10 23:32:15 +0200

    Fix default demo in dev mode

--- app/data.js
+++ app/data.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 const path = require('path');
 
-const dataFile = './demo/demo.cpuprofile';
+const dataFile = './demo/nodejs.jsonxl';
 
 module.exports = function() {
     return require('fs')
