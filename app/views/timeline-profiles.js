4f9b97c467890df4868f51d6f4b28f5698ba4499
Roman Dvornov <rdvornov@gmail.com>
2024-11-13 00:43:33 +0100

    Improve timeline-profiles view

--- app/views/timeline-profiles.js
+++ app/views/timeline-profiles.js
@@ -1,7 +1,9 @@
-discovery.view.define('timeline-profiles', function(el, config, data, context) {
-    const profiles = Array.isArray(data) ? data : [];
-    const min = discovery.query('startTime.min() or 0', profiles);
-    const max = discovery.query('endTime.max() or 0', profiles);
+discovery.view.define('timeline-profiles', function(el, props, data, context) {
+    const profiles = props.profiles || (Array.isArray(data) ? data : []);
+    const min = props.startTime || props.startTime === 0
+        ? props.startTime
+        : discovery.query('startTime.min() or 0', profiles);
+    const max = props.endTime || discovery.query('endTime.max() or 0', profiles);
     const range = max - min;
 
     el.style.setProperty('--range', range);
@@ -12,6 +14,21 @@ discovery.view.define('timeline-profiles', function(el, config, data, context) {
         barEl.className = 'profile';
         barEl.style.setProperty('--x1', (profile.startTime - min) / range);
         barEl.style.setProperty('--x2', (profile.endTime - min) / range);
+
+        if (profile.timeDeltasByProfile) {
+            const total = profile.timeDeltasByProfile.reduce((s, n) => s + n, 0);
+            for (let i = profile.timeDeltasByProfile.length - 1, start = 0; i >= 0; i--) {
+                const presenceEl = document.createElement('div');
+                const duration = profile.timeDeltasByProfile[i];
+
+                presenceEl.style.setProperty('--x1', start / total);
+                presenceEl.style.setProperty('--x2', (start + duration) / total);
+
+                barEl.append(presenceEl);
+
+                start += duration;
+            }
+        }
 
         if (context.data.currentProfile?.samples === profile.samples) {
             barEl.classList.add('selected');
@@ -30,12 +47,12 @@ discovery.view.define('timeline-profiles', function(el, config, data, context) {
             'text:name',
             { view: 'block', content: 'text:"Runtime: " + runtime.name' },
             'html:"<hr>"',
-            { view: 'block', content: 'text:"Profile time: " + totalTime.ms()' },
-            { view: 'block', content: 'text:"Samples: " + sourceInfo.samples' },
-            { view: 'block', content: 'text:"Sampling interval: " + sourceInfo.samplesInterval' },
+            { view: 'block', content: 'text-numeric:"Profile time: " + totalTime.ms()' },
+            { view: 'block', content: 'text-numeric:"Samples: " + sourceInfo.samples' },
+            { view: 'block', content: 'text-numeric:"Sampling interval: " + sourceInfo.samplesInterval' },
             'html:"<hr>"',
-            { view: 'block', content: 'text:"Call tree nodes: " + sourceInfo.nodes' },
-            { view: 'block', content: 'text:"Call frames: " + callFrames.size()' }
+            { view: 'block', content: 'text-numeric:"Call tree nodes: " + sourceInfo.nodes' },
+            { view: 'block', content: 'text-numeric:"Call frames: " + callFrames.size()' }
         ], profile, context);
     }
 });
