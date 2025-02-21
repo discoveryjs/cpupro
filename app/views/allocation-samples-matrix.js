d18edf7f9b5f773c2d03a0f1dd016963c0d14188
Roman Dvornov <rdvornov@gmail.com>
2025-02-21 16:25:54 +0100

    Tweak footer in allocation-samples-matrix

--- app/views/allocation-samples-matrix.js
+++ app/views/allocation-samples-matrix.js
@@ -6,23 +6,20 @@ const metrics = [
     { key: 'long-lived', header: 'Long lived' },
     { key: 'short-lived', header: 'Short lived' }
 ];
+const metricView = [
+    'text-with-unit{ value: sum > 999 ? sum.unit() : sum + "b", unit: true }',
+    {
+        view: 'block',
+        className: 'metric-stat',
+        content: 'text-numeric:`${min != max ? min + "…" : ""}${max.bytes()} × ${count}`'
+    }
+];
 const metricCells = metrics.map(({ key, header }) => ({
     header,
     className: 'metric-cell lifespan-' + key,
     sorting: `$["${key}"].sum desc`,
     data: `$["${key}"]`,
-    content: {
-        view: 'block',
-        when: 'sum',
-        content: [
-            'text-with-unit{ value: sum > 999 ? sum.unit() : sum + "b", unit: true }',
-            {
-                view: 'block',
-                className: 'metric-stat',
-                content: 'text-numeric:`${min != max ? min + "…" : ""}${max.bytes()} × ${count}`'
-            }
-        ]
-    }
+    content: metricView
 }));
 
 discovery.view.define('allocation-samples-matrix', {
@@ -51,16 +48,21 @@ discovery.view.define('allocation-samples-matrix', {
         rowEl.append(
             createElement('td', 'view-table-cell', 'All types'),
             ...metrics.map(({ key }) => {
-                const sum = data.reduce((sum, entry) => sum + (entry[key]?.sum ?? 0), 0);
                 const cellEl = createElement('td', 'view-table-cell metric-cell lifespan-' + key);
+                const acc = data.reduce(
+                    (acc, { [key]: col }) => col
+                        ? {
+                            sum: acc.sum + col.sum,
+                            count: acc.count + col.count,
+                            min: Math.min(acc.min, col.min),
+                            max: Math.max(acc.max, col.max)
+                        }
+                        : acc,
+                    { sum: 0, count: 0, min: Infinity, max: 0 }
+                );
 
-                if (sum) {
-                    discovery.view.render(
-                        cellEl,
-                        'text-with-unit{ value: sum > 999 ? sum.unit() : sum + "b", unit: true }',
-                        { sum },
-                        context
-                    );
+                if (acc.sum) {
+                    discovery.view.render(cellEl, metricView, acc, context);
                 }
 
                 return cellEl;
