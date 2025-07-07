06cb2572e7244b4b9bb6be015dea4fab2e272c6c
Roman Dvornov <rdvornov@gmail.com>
2025-07-07 18:13:43 +0200

    Refactor locations page

--- app/pages/locations.js
+++ app/pages/locations.js
@@ -1,48 +1,38 @@
-import { allocTypes } from '../prepare/const';
 import { timingCols } from './common.js';
 
 discovery.page.define('locations', {
-    view: 'switch',
+    view: 'context',
     context: '{ ...#, currentProfile }',
     data: 'currentProfile.callFramePositionsTimingsFiltered.entries.sort(selfTime desc, totalTime desc)',
-    content: [
-        { when: 'no $', content: {
-            view: 'alert-warning',
-            content: 'md:"No call frame with id \\"{{#.id}}\\" is found\\n\\n[Back to index page](#)"'
-        } },
-        { content: [{
-            view: 'context',
-            modifiers: [
-            ],
-            content: {
-                view: 'table',
-                cols: [
-                    ...timingCols,
-                    {
-                        header: 'Location code',
-                        content: 'location-source:entry'
-                    },
-                    {
-                        header: '',
-                        context: '{ ...#, locationLine: entry.callFrame.script.source[entry.callFrame.start:entry.scriptOffset].split(/\\r\\n?|\\n/).size() }',
-                        data: 'entry.callFrame',
-                        contentWhen: 'hasSource()',
-                        details: {
-                            view: 'call-frame-source',
-                            postRender(el, _, data, context) {
-                                const numEl = el.querySelector(`:scope .view-source__lines span:nth-child(${context.locationLine})`);
-                                if (numEl) {
-                                    numEl.classList.add('selected');
-                                }
-                            }
+    modifiers: [
+    ],
+    content: {
+        view: 'table',
+        cols: [
+            ...timingCols,
+            {
+                header: 'Location code',
+                content: 'location-source:entry'
+            },
+            {
+                header: '',
+                context: '{ ...#, locationLine: entry.callFrame.script.source[entry.callFrame.start:entry.scriptOffset].split(/\\r\\n?|\\n/).size() }',
+                data: 'entry.callFrame',
+                contentWhen: 'hasSource()',
+                details: {
+                    view: 'call-frame-source',
+                    postRender(el, _, data, context) {
+                        const numEl = el.querySelector(`:scope .view-source__lines span:nth-child(${context.locationLine})`);
+                        if (numEl) {
+                            numEl.classList.add('selected');
                         }
-                    },
-                    {
-                        header: 'Call frame',
-                        content: 'call-frame-badge:entry.callFrame'
                     }
-                ]
+                }
+            },
+            {
+                header: 'Call frame',
+                content: 'call-frame-badge:entry.callFrame'
             }
-        }] }
-    ]
+        ]
+    }
 });
